4ad2b05953a913a862ac0078fd79a64140a47a29
Vi Ta <42556132+Tavi66@users.noreply.github.com>
2020-12-15 21:04:07 -0600

    Revert "Day35Q5"

--- init_query/init.sql
+++
@@ -1,4 +0,0 @@
-drop table if exists item;
-drop table if exists itemType;
-create table itemType(id int(11) primary key,detail varchar(255),name varchar(255));
-create table item(id int(11) primary key,cost double,name varchar(255),itemTypeId int(11),foreign key(itemTypeId) references itemType(id));
